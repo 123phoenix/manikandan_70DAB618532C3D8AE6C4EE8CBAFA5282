d313b8b517baf95e07e24c2c1936b75fbc4602a2
123phoenix <144211341+123phoenix@users.noreply.github.com>
2023-09-06 00:23:27 -0700

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,10 @@
+city = "chicago"
+
+if city == "New york":
+    print("Wellcome to New york!")
+elif city == "Los Angeles" :
+    print("Wellcome to Los Angeles!")
+else:
+    print("Wellcome to", city + "!")
+
+  
